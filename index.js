b5a1959f99d8319fa4c0e1dd7ae8c8038512ecd2
aclem43 <aclemeur@gmail.com>
2023-03-28 09:38:23 +1000

    Added Sprightly

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const sprightlyExpress = require("sprightly/express");
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.engine(
+  "html",
+  sprightlyExpress({
+    cache: false,
+    keyFallback: "obada",
+    throwOnKeyNotfound: false,
+  })
+);
 
 app.use(express.static("static"));
+app.get("/", (_, res) => {
+  const data = {
+    testData: "This is data from the server",
+  };
+  res.render("../src/index.html", data);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
